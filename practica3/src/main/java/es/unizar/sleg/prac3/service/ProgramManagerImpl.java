62f597b34978556a0f80e44b6f4e179bbc4ee584
dbarelop <dbarelop@gmail.com>
2016-12-06 20:17:45 +0100

    Elminado repositorio (funcionalidad en servicio), documentado problema con librería tess4j en OSX, añadida configuración para dosbox, corregidos ficheros binarios de datos

--- practica3/src/main/java/es/unizar/sleg/prac3/service/ProgramManagerImpl.java
+++ practica3/src/main/java/es/unizar/sleg/prac3/service/ProgramManagerImpl.java
@@ -1,20 +1,49 @@
 package es.unizar.sleg.prac3.service;
 
 import es.unizar.sleg.prac3.domain.Program;
-import es.unizar.sleg.prac3.repository.ProgramRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract1;
 import org.springframework.stereotype.Service;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Service
 public class ProgramManagerImpl implements ProgramManager {
 
-    @Autowired
-    private ProgramRepository programRepository;
+    private static final Logger logger = Logger.getLogger(ProgramManager.class.getName());
+
+    private Robot robot;
+    private ITesseract ocr;
+
+    public ProgramManagerImpl() {
+        try {
+            startDatabase();
+            robot = new Robot();
+            ocr = new Tesseract1();
+            ocr.setLanguage("spa");
+        } catch (IOException | AWTException e) {
+            logger.log(Level.SEVERE, e.getMessage(), e);
+        }
+    }
+
+    private void startDatabase() throws IOException {
+        Process dosbox = Runtime.getRuntime().exec("dosbox msdos-database/gwbasic.bat -noconsole");
+    }
+
+    private void readScreen() {
+        final int x1 = 0, x2 = 300, y1 = 0, y2 = 200;
+        BufferedImage capture = robot.createScreenCapture(new Rectangle(x1, y1, x2 - x1, y2 - y1));
+
+    }
 
     @Override
     public List<Program> getPrograms() {
-        return programRepository.getPrograms();
+        return null;
     }
 }
