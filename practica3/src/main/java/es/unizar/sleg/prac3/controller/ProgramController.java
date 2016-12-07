fed8840172c9548b5d81dcfb2a5906dc2bdc3247
dbarelop <dbarelop@gmail.com>
2016-12-07 16:21:44 +0100

    Añadidos métodos al controlador para acceder al servicio

--- practica3/src/main/java/es/unizar/sleg/prac3/controller/ProgramController.java
+++ practica3/src/main/java/es/unizar/sleg/prac3/controller/ProgramController.java
@@ -1,13 +1,37 @@
 package es.unizar.sleg.prac3.controller;
 
+import es.unizar.sleg.prac3.domain.Program;
 import es.unizar.sleg.prac3.service.ProgramManager;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
 public class ProgramController {
 
     @Autowired
     private ProgramManager programManager;
 
+    @GetMapping("/programs")
+    public ResponseEntity<List<Program>> getPrograms() {
+        List<Program> programs = programManager.getPrograms();
+        return new ResponseEntity<>(programs, HttpStatus.OK);
+    }
+
+    @GetMapping("/program/{id}")
+    public ResponseEntity<Program> getProgram(@PathVariable("id") Integer id) {
+        Program p = programManager.getProgram(id);
+        return new ResponseEntity<>(p, HttpStatus.OK);
+    }
+
+    @GetMapping("/program/name/{name}")
+    public ResponseEntity<Program> getProgram(@PathVariable("name") String name) {
+        Program p = programManager.getProgram(name);
+        return new ResponseEntity<>(p, HttpStatus.OK);
+    }
 }
