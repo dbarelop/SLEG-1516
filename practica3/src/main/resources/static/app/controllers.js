d38f904a26edc5e047cdbf74723fcd18ea3816aa
dbarelop <dbarelop@gmail.com>
2017-01-14 19:41:08 +0100

    Añadido alert mientras se usa DOSBox

--- practica3/src/main/resources/static/app/controllers.js
+++ practica3/src/main/resources/static/app/controllers.js
@@ -1,38 +1,52 @@
 angular.module("Prac3SLEGApp.controllers", []).controller("ProgramsCtrl", function($scope, ProgramsService) {
 
+    $scope.dosbox = false;
+
     $scope.programId = "";
     $scope.programName = "";
     $scope.numPrograms = "";
 
     $scope.getNumPrograms = function() {
+        $scope.dosbox = true;
         ProgramsService.getNumPrograms().then(function(data) {
             $scope.numPrograms = data;
         }, function(err) {
             console.log(err);
+        }).finally(function() {
+            $scope.dosbox = false;
         });
     };
 
     $scope.getPrograms = function() {
+        $scope.dosbox = true;
         ProgramsService.getPrograms().then(function(data) {
             $scope.result = data;
         }, function(err) {
             console.log(err);
+        }).finally(function() {
+            $scope.dosbox = false;
         });
     };
 
     $scope.search = function() {
+        $scope.dosbox = true;
         ProgramsService.getProgram($scope.programId).then(function(data) {
             $scope.result = data;
         }, function(err) {
             console.log(err);
+        }).finally(function() {
+            $scope.dosbox = false;
         });
     };
 
     $scope.searchByName = function() {
+        $scope.dosbox = true;
         ProgramsService.getProgramByName($scope.programName).then(function(data) {
             $scope.result = data;
         }, function(err) {
             console.log(err);
+        }).finally(function() {
+            $scope.dosbox = false;
         });
     };
 
